6f2b9d46bbf14de95f8caaad951b61993c73b356
Nvbittencourt <50023449+Nvbittencourt@users.noreply.github.com>
2021-04-19 22:51:26 -0300

    Add files via upload

---
+++ funcoes.R
@@ -0,0 +1,22 @@
+# UFPE 
+# PPGCP
+# Análise de Dados
+# Professor: Hugo Medeiros
+# Aluna: Nathália Viviani Bittencourt
+# Atividade: Funcoes
+
+# função
+f <- function() {
+  cat("Hello, world!\n") # funcao print com quebra de linha
+}
+f()
+
+# função com estrutura condicional e de repetição
+f <- function(nro) {
+  if(nro < 10) {
+  for(i in 1:nro) {
+  cat("Hello, world!\n")}} else {cat("me poupe")}}
+f(9)
+f(10)
+
+
